6370bb3f57b8c89c539a4951b021e286e29266d5
Nicolas Andres Kurek <131825518+NinAndres@users.noreply.github.com>
2024-07-31 20:59:47 -0300

    Projeto exemplo created

---
+++ src/test/java/com/exemplofaculdade/faculdade/Exemplo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplofaculdade.faculdade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exemplo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
